bc6b937ac4a66bc063fd400314cca013f99edc94
PrinceJalil <mjalilurokhman16@gmail.com>
2024-05-11 14:42:32 +0700

    First

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-gray-400">
+      
+    <p>Hello Haromain
+
+    </p>
+    </div>
+    </>
+  );
+}
